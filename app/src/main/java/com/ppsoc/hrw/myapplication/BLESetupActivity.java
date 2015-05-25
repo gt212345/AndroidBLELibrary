c84f622508537ca6fb48ee260e57ad03c346ce21
Heiru Wu <wuheiru.5203@gmail.com>
2015-05-25 17:59:01 +0800

    Initial commit

---
+++ app/src/main/java/com/ppsoc/hrw/myapplication/BLESetupActivity.java
@@ -0,0 +1,33 @@
+package com.ppsoc.hrw.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+
+public class BLESetupActivity extends UnityPlayerActivity {
+    BLESetupWizard bleSetupWizard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void setUpBLE(Context context) {
+        bleSetupWizard = new BLESetupWizard(context);
+    }
+
+    public void setDeviceName(String DEVICE_NAME) {
+        bleSetupWizard.setDeviceName(DEVICE_NAME);
+    }
+
+    public void setOnDataTransferListener (OnDataTransferListener listener) {
+        bleSetupWizard.setOnDataTransferListener(listener);
+    }
+
+    public void startScan(int millisecond){
+        bleSetupWizard.startScanning(millisecond);
+    }
+
+}
---
+++ app/src/main/java/com/ppsoc/hrw/myapplication/OnDataTransferListener.java
@@ -0,0 +1,8 @@
+package com.ppsoc.hrw.myapplication;
+
+/**
+ * Created by User on 2015/5/25.
+ */
+public interface OnDataTransferListener {
+    void onDataTransfer(int x, int y, int z);
+}
